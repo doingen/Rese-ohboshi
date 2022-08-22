5b47357d434b3e0b5866a04a951e9b8cfc79685f
doingen <ee_v_chiacky@yahoo.co.jp>
2022-08-22 10:25:29 +0900

    mix

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,14 +4,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @if(app('env')=='local')
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="stylesheet" href="/css/layout.css">
-  @endif
-  @if(app('env')=='production')
-    <link rel="stylesheet" href="{{ secure_asset('/css/reset.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('/css/layout.css') }}">
-  @endif
+  <link rel="stylesheet" href="{{mix('css/reset.css')}}">
+  <link rel="stylesheet" href="{{mix('css/layout.css')}}">
 </head>
 <body>
   <header>
